bbb760b48b3c0ef8e5ffa534597ef5c9bdfa9b5a
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2023-08-28 05:05:48 +0300

    HomeView.vue -> NFC icon added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="home">
+
+    <div class="waveStrength-3">
+      <div class="wv3 wave" style="">
+        <div class="wv2 wave" style="">
+          <div class="wv1 wave">
+          </div>
+        </div>
+      </div>
+    </div>
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,6 +18,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 export default {
   name: 'HomeView',
@@ -16,3 +27,35 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.waveStrength-3{
+  /*transform: rotate(90deg);*/
+  margin-left: auto;
+  margin-right: auto;
+
+}
+
+.wave {
+  display: inline-block;
+  border: 25px solid transparent;
+  border-top-color: currentColor;
+  border-radius: 50%;
+  border-style: solid;
+  margin: 5px;
+}
+
+
+.waveStrength-3 .wv4.wave,
+.waveStrength-2 .wv4.wave,
+.waveStrength-2 .wv3.wave,
+.waveStrength-1 .wv4.wave,
+.waveStrength-1 .wv3.wave,
+.waveStrength-1 .wv2.wave {
+  border-top-color: #eee;
+
+}
+
+
+</style>
