fec67c418a6ff09f164c1d0ad3137c9969e1f761
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2023-08-31 14:06:47 +0300

    ContactObject.vue -> you can add networkslist, edit. It saves into the cookies

--- src/components/ContactObject.vue
+++ src/components/ContactObject.vue
@@ -1,22 +1,23 @@
 <template>
 <div>
-  <div v-if="editing" class="card">
+  <div v-if="createNew" class="card">
+    <button @click="addNew" class="btn btn-secondary mt-2">create new</button>
+  </div>
+  <div v-else-if="editing" class="card">
     <textarea v-model="editedText" rows="4" class="form-control"></textarea>
-    <div class="input-group input-group-lg">
+    <div class="input-group input-group-lg" v-for="socialNetwork in this.listOfNetworks" v-bind="socialNetwork">
       <div class="input-group-prepend">
-        <span class="input-group-text" id="inputGroup-sizing-default">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
-          </svg>
+        <span class="input-group-text" id="inputGroup-sizing-default" v-html="socialNetwork.icon">
         </span>
       </div>
-      <input type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
+      <input type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" v-model="this.valuesOfSocialNetworkList[socialNetwork.title]">
     </div>
 
     <button @click="saveEdit" class="btn btn-primary mt-2">Save</button>
     <button @click="cancelEdit" class="btn btn-secondary mt-2">Cancel</button>
   </div>
   <div v-else class="card">
+
     <div class="dropdown" align="right">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
@@ -30,6 +31,17 @@
     </div>
     <h5 class="card-title">{{ cardTitle }}</h5>
     <p class="card-text">Last update: {{ lastUpdate }}</p>
+
+    <div class="card-text" v-for="(value, key) in this.valuesOfSocialNetworkList" :key="key">
+      <div class="input-group input-group-lg">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-default" v-html="listOfNetworks.find(network => network.title === key).icon">
+        </span>
+        </div>
+        {{value}}
+      </div>
+    </div>
+
   </div>
 </div>
 </template>
@@ -40,15 +52,28 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 export default {
   name: "ContactObject",
   props: {
-    id: Number
+    id: Number,
+    createNew: Boolean
   },
   data() {
     return {
       editing: false,
-      editedText: "", // For holding the edited text
-      cardTitle: "Title", // Default title
-      lastUpdate: "28.08.2023 5:22" // Default last update
+      editedText: "",
+      cardTitle: "Title",
+      lastUpdate: "28.08.2023 5:22",
+      valuesOfSocialNetworkList: {
+
+      }
     };
+  },
+
+  mounted() {
+    console.log(this.createNew === 0)
+    console.log(this.editing === 0)
+    if (this.createNew === false && this.editing === false) {
+      console.log("create new")
+      this.getInformationAboutContact();
+    }
   },
   methods: {
     startEdit() {
@@ -58,11 +83,34 @@ export default {
     saveEdit() {
       this.cardTitle = this.editedText;
       this.editing = false;
+      this.saveChanges();
     },
     cancelEdit() {
       this.editing = false;
-    }
-  }
+    },
+    createNewCard() {
+
+    },
+    getListOfAllContacts(){
+      var result = (document.cookie.match(new RegExp("listOfAllContacts" + '=([^;]+)')) || [])[1] && JSON.parse((document.cookie.match(new RegExp("listOfAllContacts" + '=([^;]+)')) || [])[1]) || [];
+      console.log(result);
+      return result;
+    },
+    addNew(){
+      var listOfAllContacts = this.getListOfAllContacts();
+      listOfAllContacts.push({id:listOfAllContacts.length, valuesOfSocialNetworkList: this.valuesOfSocialNetworkList});
+      document.cookie = "listOfAllContacts" + "=" + JSON.stringify(listOfAllContacts) + "; path=/";
+    },
+    saveChanges(){
+      var listOfAllContacts = this.getListOfAllContacts();
+      console.log(this.valuesOfSocialNetworkList)
+      listOfAllContacts[this.id]["valuesOfSocialNetworkList"]= this.valuesOfSocialNetworkList;
+      document.cookie = "listOfAllContacts" + "=" + JSON.stringify(listOfAllContacts) + "; path=/";
+    },
+  getInformationAboutContact(){
+    this.valuesOfSocialNetworkList = this.getListOfAllContacts().filter(contact => contact.id === this.id)[0]["valuesOfSocialNetworkList"];
+  },
+  },
 }
 </script>
 
