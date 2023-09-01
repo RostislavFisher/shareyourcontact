2682bdcbcb70eda933ce1e98b99ab133488ad6a0
RostislavFisher <59475110+RostislavFisher@users.noreply.github.com>
2023-09-01 18:50:57 +0300

    HomeView.vue -> HelloWorld.vue deleted HelloWorld.vue -> deleted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,20 +23,16 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import ContactObject from "@/components/ContactObject";
 export default {
   name: 'HomeView',
   components: {
     ContactObject,
-    HelloWorld
   },
   data() {
     return {
